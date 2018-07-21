ad30c3683aea101ba3e56344dc58e11e1f779e58
Munachiso Ilokah <munachisoilokah@gmail.com>
2018-07-20 20:52:27 -0400

    modified encryption test.

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -5,8 +5,9 @@ from src.abe_schemes.abe_symmetric_file_encryption import *
 
 
 def test_verify_file_content():
-    with open('test_data/sample_file.txt') as f:
+    with open('test_data/test_file.txt', 'r') as f:
         content = f.readlines()
+    f.close()
     actual = 'This is a sample file to test encryption and decryption modules.\n'
     assert content[0] == actual
     assert len(content) == 1
